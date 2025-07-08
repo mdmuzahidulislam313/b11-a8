54bf74022bc664538937703c82d7d4b4003ca09e
muzahidulislamhadi <muzahidulislamhadi@gmail.com>
2025-07-08 23:19:07 +0600

    3rd

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
   },
-});
+})
